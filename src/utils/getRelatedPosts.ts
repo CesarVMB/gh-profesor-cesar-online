b94840a3d2e7705a5808612e9289d0622263dfb0
César Gabriel <cesargabriel@gmail.com>
2026-01-22 18:17:01 -0500

    Added Related Posts component.

---
+++ src/utils/getRelatedPosts.ts
@@ -0,0 +1,12 @@
+export function getRelatedPosts(allPosts: any[], currentPost: any, limit = 3) {
+  return allPosts
+    .filter((post) => post.slug !== currentPost.slug)
+    .filter(
+      (post) =>
+        post.data.category === currentPost.data.category ||
+        (post.data.tags &&
+          currentPost.data.tags &&
+          post.data.tags.some((tag) => currentPost.data.tags.includes(tag))),
+    )
+    .slice(0, limit);
+}
