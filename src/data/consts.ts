3328dc383afbe5f485e3708bbe7ccf4e58aac522
César Gabriel <cesargabriel@gmail.com>
2026-01-22 21:57:42 -0500

    Added more components and compatibility with LaTeX.

---
+++ src/data/consts.ts
@@ -0,0 +1,19 @@
+const SITE = {
+  language: "es",
+  title: "Profesor César Online",
+  description:
+    "Clases de matemáticas, física y química 100 % online. Dirigidas a estudiantes de 3º, 4º y 5º de secundaria y Bachillerato Internacional (IB).",
+  slogan: "Matemáticas, física y química",
+};
+
+const COPYRIGHT = {
+  owner: "César Vargas-Machuca",
+  notice: "Todos los derechos reservados",
+};
+
+const LOCATION = {
+  city: "Lima",
+  country: "Perú",
+};
+
+export { SITE, COPYRIGHT, LOCATION };
---
+++ src/scripts/js/global.js
@@ -0,0 +1,2 @@
+let jCurrentYear = document.getElementById("jCurrentYear");
+jCurrentYear.innerText = new Date().getFullYear();
