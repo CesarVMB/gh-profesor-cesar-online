ed5a3679e5a742d5b64fc25e688d0d0fe5c2cad7
César Gabriel <cesargabriel@gmail.com>
2026-01-23 15:22:55 -0500

    Added active navigation link.

--- public/js/global.js
+++ public/js/global.js
@@ -1,1 +1,3 @@
+// GET THE ACTIVE PAGE
+document.querySelectorAll(".nav-item > a.nav-link").forEach(link=>{if(link.href===window.location.href){console.log(link.href);link.classList.add("active");link.setAttribute("aria-current","page")}});document.querySelectorAll(".dropdown-menu > li > a").forEach(link=>{if(link.href===window.location.href){console.log(link.href);link.classList.add("active");link.setAttribute("aria-current","page")}});// SHOW THE CURRENT YEAR
 let jCurrentYear=document.getElementById("jCurrentYear");jCurrentYear.innerText=new Date().getFullYear();
